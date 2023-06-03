aa7e1a4d1faa504788b696c4e7a3adb77c44b691
Sarita Kharabe <98185682+saritakharabe@users.noreply.github.com>
2023-06-03 16:59:08 -0400

    created model,login,& Signup

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const Comments = require('./Comments');
+const Like = require('./Like');
+const Profile = require('./Profile');
+const Post = require('./Post');
+
+module.exports = { User, Comments, Like, Profile, Post };
